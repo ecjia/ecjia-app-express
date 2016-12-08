36dc27fd196d95f14121f4c14e215a3372d749a3
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-08 17:57:48 +0800

    取货后更新配送状态

--- modules/express/pickup_module.class.php
+++ modules/express/pickup_module.class.php
@@ -15,12 +15,13 @@ class pickup_module extends api_admin implements api_interface {
         $delivery_sn = $this->requestData('delivery_sn');
         
 		if (empty($delivery_sn)) {
-    		return new ecjia_error( 'invalid_parameter', RC_Lang::get ('system::system.invalid_parameter' ));
+    		return new ecjia_error('invalid_parameter', RC_Lang::get ('system::system.invalid_parameter' ));
     	}
     	$express_order = array();
     	$express_order_db = RC_Model::model('express/express_order_viewmodel');
     	$where = array('staff_id' => $_SESSION['staff_id'], 'eo.delivery_sn' => $delivery_sn);
-    	$express_order_info = $express_order_db->join(array('delivery_order', 'order_info'))->where($where)->find();
+    	$field = 'eo.*, oi.add_time as order_time, oi.pay_time, oi.order_amount, oi.pay_name, sf.merchants_name, sf.address as merchant_address, sf.longitude as merchant_longitude, sf.latitude as merchant_latitude';
+    	$express_order_info = $express_order_db->field($field)->join(array('delivery_order', 'order_info', 'store_franchisee'))->where($where)->find();
 		
     	if (empty($express_order_info)) {
     		return new ecjia_error('express_no_exists_error', '此配送单不存在！');
@@ -74,6 +75,8 @@ class pickup_module extends api_admin implements api_interface {
     			);
     		}
     	}
+    	
+    	RC_Model::model('express/express_order_model')->where($where)->update(array('status' => 2));
     	
 		return $express_order;
 	 }	
