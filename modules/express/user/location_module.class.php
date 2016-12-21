19b333d6530d86fd5343abbf8499b0116d1a8d40
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-21 17:15:30 +0800

    快递配送数量与距离更新提交

--- modules/express/user/location_module.class.php
+++ modules/express/user/location_module.class.php
@@ -20,8 +20,12 @@ class location_module extends api_admin implements api_interface {
 		}
 		
 		$express_model_db = RC_Model::model('express/express_user_model');
-		
-		$express_model_db->where(array('user_id' => $_SESSION['staff_id']))->update(array('longitude' => $location['longitude'], 'latitude' => $location['latitude']));
+		$express_user_info = $express_model_db->where(array('user_id' => $_SESSION['staff_id']))->find();
+		if (empty($express_user_info)) {
+			$express_model_db->insert(array('user_id' => $_SESSION['staff_id'], 'store_id' => $_SESSION['store_id'], 'longitude' => $location['longitude'], 'latitude' => $location['latitude'], 'delivery_count' => 0, 'delivery_distance' => 0));
+		} else {
+			$express_model_db->where(array('user_id' => $_SESSION['staff_id']))->update(array('longitude' => $location['longitude'], 'latitude' => $location['latitude']));
+		}
 		
 		return array();
 
