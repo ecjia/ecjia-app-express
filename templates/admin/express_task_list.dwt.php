062be47fe524ffe78ec21eaea2c96b82f3b1b449
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-01-03 13:13:48 +0800

    样式调整

--- templates/admin/express_task_list.dwt.php
+++ templates/admin/express_task_list.dwt.php
@@ -34,12 +34,13 @@
 		<li class="{if $type eq 'wait_grab'}active{/if}"><a  href='{url path="express/admin/init" args="type=wait_grab"}'>待抢单 <span class="badge badge-info">{if $express_order_count.wait_grab}{$express_order_count.wait_grab}{else}0{/if}</span> </a></li>
 		<li class="{if $type eq 'wait_pickup'}active{/if}"><a  href='{url path="express/admin/wait_pickup" args="type=wait_pickup"}'>待取货 <span class="badge badge-info">{if $express_order_count.wait_pickup}{$express_order_count.wait_pickup}{else}0{/if}</span> </a></li>
 		<li class="{if $type eq 'sending'}active{/if}"><a  href='{url path="express/admin/wait_pickup" args="type=sending"}'>配送中 <span class="badge badge-info">{if $express_order_count.sending}{$express_order_count.sending}{else}0{/if}</span> </a></li>
-		<!-- {if $express_order_count.wait_grab} -->
-			<li class="map-change-remark map-exp-order" style="float:left;margin-top:8px;">注：配送单号&nbsp;&nbsp;<span class="mark order">[{$first_express_order.express_sn}]</span>&nbsp;&nbsp;位置</li>
-			<li class="map-change-remark map-exp-user" style="float:left;margin-top:8px;">注：配送员&nbsp;&nbsp;<span class="mark user">[{$express_info.name}]</span>&nbsp;&nbsp;位置</li>
-		<!--{/if} -->
-		
-		<li class="countdown" style="float:left;padding-left:7%;"><span style="color:#c62626;" id='numDiv'>120</span>&nbsp;&nbsp;秒后自动刷新 </li><a class="btn" style="background: #058DC7;text-shadow:none;margin-left:1%;margin-top:4px;" href='{url path="express/admin/init" args="type=wait_grab"}'><span style="color:#fff;">手动刷新</span> </a>
+			<form class="f_r form-inline" style="width:49%;">
+			<!-- {if $express_order_count.wait_grab} -->
+				<div class="map-change-remark map-exp-order">注：配送单号&nbsp;&nbsp;<span class="mark order">[{$first_express_order.express_sn}]</span>&nbsp;&nbsp;位置</div>
+				<div class="map-change-remark map-exp-user">注：配送员&nbsp;&nbsp;<span class="mark user">[{$express_info.name}]</span>&nbsp;&nbsp;位置</div>
+			<!--{/if} -->
+			<div class="fresh"><span class="numcolor" id='numDiv'>120</span>&nbsp;&nbsp;秒后自动刷新<a class="btn" style="background: #058DC7;text-shadow:none;margin-left:3%;margin-top:1px;" href='{url path="express/admin/init" args="type=wait_grab"}'><span style="color:#fff;">手动刷新</span> </a></div>
+		</form>
 	</ul>
 </div>
 
